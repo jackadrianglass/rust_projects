f4227ec49b1f3dc4b8374b0527b72618d1ec5d93
glassjack <jackadrianglass@gmail.com>
2022-10-04 21:45:50 -0600

    [NoC] Fixed the perlin noise generation example
    Note to self for next time though
    * srgb refers to the values between 0 and 1
    * rgb8 refers to the values between 0 and 255
    * if it's a colour value that's a float, it's probably 0-1

--- nature_of_code/perlin_basic/src/main.rs
+++ nature_of_code/perlin_basic/src/main.rs
@@ -1,7 +1,6 @@
-use std::time::{UNIX_EPOCH, SystemTime};
 use nannou::{
     color,
-    noise::{NoiseFn, Perlin, Seedable},
+    noise::{utils::*, Fbm},
     prelude::*,
 };
 
@@ -14,18 +13,28 @@ fn view(app: &App, frame: Frame) {
         return;
     }
 
-    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-    let perlin = Perlin::new().set_seed(now as u32);
     let window = app.window_rect();
+    let fbm = Fbm::new();
+
+    let map = PlaneMapBuilder::new(&fbm)
+        .set_size(1024, 768)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build();
+
 
     let draw = app.draw();
-    let d = 5;
+    let d = 10;
     for x in (window.left() as i32..window.right() as i32).step_by(d) {
         for y in (window.bottom() as i32..window.top() as i32).step_by(d) {
+            let x_val = (x as f32 + window.right()) as usize;
+            let y_val = (y as f32 + window.top()) as usize;
             draw.rect()
                 .xy(Vec2::new(x as f32, y as f32))
                 .wh(Vec2::new(d as f32, d as f32))
-                .color(color::gray(255.0 * perlin.get([(x as f32 / window.right()) as f64, (y as f32 / window.top()) as f64])));
+                .color(color::gray(
+                    (map.get_value(x_val, y_val) + 1.0) / 2.0,
+                ));
         }
     }
     draw.to_frame(app, &frame).unwrap();
