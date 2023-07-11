93fc86c5d898a20977f725c5b38c19781276431a
glassjack <jackadrianglass@gmail.com>
2023-07-10 21:31:02 -0600

    [gl] Created a spinning triangle from glium tutorials

--- opengl/hello_triangle/src/main.rs
+++ opengl/hello_triangle/src/main.rs
@@ -1,6 +1,13 @@
+#[macro_use]
 extern crate glium;
 
 use glium::Surface;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
 
 fn main() {
     use glium::glutin;
@@ -10,22 +17,76 @@ fn main() {
     let context_builder = glutin::ContextBuilder::new();
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        let mut target = display.draw();
-        target.clear_color(0.0, 1.0, 0.0, 1.0);
-        target.finish().unwrap();
-        let next_frame = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame);
-        match event {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
-        *control_flow = glutin::event_loop::ControlFlow::Exit;
-        return;
-        },
-            _ => return,
-            },
-                _ => (),
-        }
+    let vert1 = Vertex{ position: [ -0.5, 0.5 ]};
+    let vert2 = Vertex{ position: [ 0.25, -0.5 ]};
+    let vert3 = Vertex{ position: [ 0.5, 0.5 ]};
+    let shape = vec![vert1, vert2, vert3];
 
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+    #version 140
+    in vec2 position;
+    uniform mat4 matrix;
+
+    void main() {
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+    }
+    "#;
+
+    let fragment_shader_src = r#"
+    #version 140
+
+    out vec4 color;
+
+    uniform mat4 matrix;
+
+    void main() {
+        color = (matrix * vec4(1.0, 1.0, 1.0, 1.0) + vec4(1.0, 1.0, 1.0, 1.0)) / 2.0;
+    }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut time: f32 = 0.0;
+    event_loop.run(move |loop_event, _, control_flow| {
+        match loop_event {
+            glutin::event::Event::WindowEvent { event, .. } => {
+                match event {
+                    glutin::event::WindowEvent::CloseRequested => {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    },
+                    glutin::event::WindowEvent::Resized(window_size) => {
+                        display.gl_window().window().set_inner_size(window_size);
+                    },
+                    _ => (),
+                }
+            },
+            glutin::event::Event::RedrawEventsCleared => {
+                display.gl_window().window().request_redraw();
+            },
+            glutin::event::Event::RedrawRequested(_) => {
+                time += 0.0005;
+
+                let mut target = display.draw();
+                let uniforms = uniform!{
+                    matrix: [
+                        [ time.cos(), time.sin(), 0.0, 0.0 ],
+                        [-time.sin(), time.cos(), 0.0, 0.0 ],
+                        [0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0],
+                    ]
+                };
+                target.clear_color(0.43921568627, 0.50196078431, 0.56470588235, 1.0);
+
+                target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+
+                target.finish().unwrap();
+            },
+            _ => return,
+        }
     });
 }
